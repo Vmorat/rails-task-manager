13d35ae0b8fed35ab9697eb4f416620c362978d3
valentina mora <valentina.mora@ieseg.fr>
2024-02-13 18:00:01 +0100

    Rails > Models & CRUD- Rails Task Manager - solution

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # CREATE
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # READ
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # UPDATE
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # DELETE
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
